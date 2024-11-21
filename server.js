c09ca4872ceb0f0c27cd12b3566d260f534d73d6
valentinohrynt <hariyantovalentino@gmail.com>
2024-11-22 01:25:21 +0700

    changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 
 // Routes
 app.use('/api', userRoutes);
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || '127.0.0.1';
